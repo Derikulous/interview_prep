e80cf74db80a2ca898e44475d8f55a0dc756d9fb
Derik Strattan <derik.strattan@gmail.com>
2015-03-16 20:46:21 -0700

    refactor weight converter with constant

--- java/WeightConverter.java
+++ java/WeightConverter.java
@@ -1,18 +1,21 @@
 package com.derik;
+
 import java.util.Scanner;
+
 public class Main {
   public static void main(String[] args) {
     Scanner user_input = new Scanner(System.in);
-    int exit = -1;
+
     while(true){
       System.out.println("Enter a weight in pounds. Enter -1 to exit");
       float weightInPounds = user_input.nextFloat();
       double conversionFactor = 0.453592;
-      double poundsToKiloConverter = weightInPounds*conversionFactor;
-      System.out.println(poundsToKiloConverter);
-      if (weightInPounds == exit){
+      double poundsToKiloConverter = weightInPounds * conversionFactor;
+      if (weightInPounds == EXIT_CODE){
         break;
       }
+      System.out.println(poundsToKiloConverter);
     }
   }
+  public static int EXIT_CODE = -1;
 }

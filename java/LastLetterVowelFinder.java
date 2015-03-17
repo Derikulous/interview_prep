c16a4ebcccd0d31e2a5447f6442c9e10d72595b3
Derik Strattan <derik.strattan@gmail.com>
2015-03-16 20:41:46 -0700

    refactor vowel check into function

--- java/LastLetterVowelFinder.java
+++ java/LastLetterVowelFinder.java
@@ -1,14 +1,25 @@
+package com.derik;
+
 import java.util.Scanner;
+
 public class Main {
   public static void main(String[] args) {
     Scanner in = new Scanner(System.in);
     System.out.println("Enter a name");
     String str = in.nextLine();
-    boolean vowelCheck = str.endsWith("a") || str.endsWith("e") || str.endsWith("i")|| str.endsWith("o")|| str.endsWith("u");
-    if(vowelCheck == true){
+    if(vowelCheck(str) == true){
       System.out.println("Your name is feminine in nature. Not gonna make it.");
     } else {
       System.out.println("Your name is masculine. Still not gonna make it.");
     }
   }
+
+  static boolean vowelCheck(String str){
+    if (str.endsWith("a") || str.endsWith("e") || str.endsWith("i")|| str.endsWith("o")|| str.endsWith("u")){
+      return true;
+    } else {
+      return false;
+    }
+  }
 }
+

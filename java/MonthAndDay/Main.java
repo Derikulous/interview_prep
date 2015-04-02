336ed5459fa845b9fdd7f8f7e74b005eda8c3b0a
Derik Strattan <derik.strattan@gmail.com>
2015-04-01 21:14:16 -0700

    reformat Main

--- java/MonthAndDay/Main.java
+++ java/MonthAndDay/Main.java
@@ -8,11 +8,11 @@ public class Main {
         MonthAndDay mad4 = new MonthAndDay(1, 1);
         MonthAndDay mad5 = new MonthAndDay(11, 0);
 
-        mad.computeMonthAndDayFromDaysAdded(1, 22, 350);
-        mad2.computeMonthAndDayFromDaysAdded(2, 22, 150);
-        mad3.computeMonthAndDayFromDaysAdded(3, 18, 700);
-        mad4.computeMonthAndDayFromDaysAdded(11, 30, 750);
-        mad5.computeMonthAndDayFromDaysAdded(11, 20, 20);
+        mad.computeMonthAndDayFromDaysAdded(350);
+        mad2.computeMonthAndDayFromDaysAdded(150);
+        mad3.computeMonthAndDayFromDaysAdded(700);
+        mad4.computeMonthAndDayFromDaysAdded(750);
+        mad5.computeMonthAndDayFromDaysAdded(20);
 
     }
 }

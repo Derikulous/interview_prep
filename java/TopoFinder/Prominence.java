345317479ea8214e7c7a5892ba87828dc981e49c
Derik Strattan <derik.strattan@gmail.com>
2015-05-24 20:01:51 -0700

    some refactoring. reduce loops

--- java/TopoFinder/Prominence.java
+++ java/TopoFinder/Prominence.java
@@ -3,27 +3,24 @@ package com.derik;
 public class Prominence {
     /**
      * Find the 1D topographical left col. This will be compared to the right col to determine the key col.
-     * @param location an integer that represents a peak
+     * @param index an index integer that represents a where a peak was identified in getProminence
      * @param landscape an array of integers that represent various peak heights. The lowest value is the lowest peak, and the highest value is the parent peak.
      * @return an integer that represents the left col
      */
-    public int getLeftCol(int location, int[] landscape){
+    public int getLeftCol(int index, int[] landscape) {
         int left_col = 0;
 
-        for (int i = 0; i < landscape.length; i++) {
-            if (landscape[i] == location) {
-                if (landscape[i - 1] < landscape[i] && landscape[i + 1] < landscape[i]) {
-                    for (int k = i - 1; k > -1; k--) {
-                        if (k == 0 && landscape[k] < landscape[k + 1]) {
-                            left_col = landscape[k];
-                            System.out.println("The left col is at the boundary. It may or may not be a true col.");
-                            break;
-                        } else if (landscape[k - 1] > landscape[k]) {
-                            left_col = landscape[k];
-                            break;
-                        }
-                    }
-                }
+        while (index > -1) {
+            if (index == 0) {
+                System.out.println("The left col is at the boundary. It may or may not be a true col.");
+                left_col = landscape[index];
+                break;
+            }
+            if (landscape[index - 1] < landscape[index]) {
+                index--;
+                left_col = landscape[index];
+            } else {
+                break;
             }
         }
         return left_col;
@@ -31,28 +28,24 @@ public class Prominence {
 
     /**
      * Find the 1D topographical right col. This will be compared to the left col to determine the key col.
-     * @param location an integer that represents a peak
+     * @param index an index integer that represents a where a peak was identified in getProminence
      * @param landscape an array of integers that represent various peak heights. The lowest value is the lowest peak, and the highest value is the parent peak.
      * @return an integer that represents the right col
      */
-    public int getRightCol(int location, int[] landscape){
+    public int getRightCol(int index, int[] landscape) {
         int right_col = 0;
 
-        for (int i = 0; i < landscape.length; i++) {
-            if (landscape[i] == location) {
-                if (landscape[i - 1] < landscape[i] && landscape[i + 1] < landscape[i]) {
-
-                    for (int j = i + 1; j < landscape.length; j++) {
-                        if (landscape[j] > landscape[j - 1]) {
-                            right_col = landscape[j - 1];
-                            break;
-                        } else if (j == landscape.length - 1) {
-                            System.out.println("The right col is at the boundary. It may or may not be a true col.");
-                            right_col = landscape[j];
-                            break;
-                        }
-                    }
-                }
+        while (index < landscape.length) {
+            if (index == landscape.length - 1) {
+                System.out.println("The right col is at the boundary. It may or may not be a true col.");
+                right_col = landscape[index];
+                break;
+            }
+            if (landscape[index + 1] < landscape[index]) {
+                index++;
+                right_col = landscape[index];
+            } else {
+                break;
             }
         }
         return right_col;
@@ -64,7 +57,7 @@ public class Prominence {
      * @param landscape an array of integers that represent various peak heights. The lowest value is the lowest peak, and the highest value is the parent peak.
      * @return an integer that represents the prominence of the location
      */
-    public int getProminence(int location, int[] landscape){
+    public int getProminence(int location, int[] landscape) {
         int prominence = 0;
         int key_col = 0;
         int indexInLocation = -1; //assume that the location is not a peak
@@ -74,8 +67,8 @@ public class Prominence {
         }
 
         for (int i = 0; i < landscape.length; i++) {
-            if(landscape[i] == location){
-                if (i == 0 || i == landscape.length - 1){
+            if(landscape[i] == location) {
+                if (i == 0 || i == landscape.length - 1) {
                     throw new IllegalArgumentException("The location you chose is at the edge of the landscape. Choose a different location.");
                 } else if (landscape[i - 1] < landscape[i] && landscape[i + 1] < landscape[i]) {
                     indexInLocation = i;
@@ -88,8 +81,8 @@ public class Prominence {
             throw new IllegalArgumentException("The location you chose is not a peak in this landscape. Choose a different location.");
         }
 
-        int right_col = getRightCol(location, landscape);
-        int left_col = getLeftCol(location, landscape);
+        int right_col = getRightCol(indexInLocation, landscape);
+        int left_col = getLeftCol(indexInLocation, landscape);
 
         if (right_col > left_col) {
             key_col = right_col;
@@ -113,7 +106,6 @@ public class Prominence {
 
         // begin the searching for maximum prominence at index 1 and never ending at the final index since prominence cannot occur at the edge
         for (int i = 1; i < landscape.length - 1; i++) {
-
             try {
                 int currentProminence = getProminence(landscape[i], landscape);
 
@@ -123,7 +115,6 @@ public class Prominence {
             } catch (IllegalArgumentException e) {
                 System.err.println("IllegalArgumentException: " + e.getMessage());
             }
-
         }
         System.out.println("The maximum prominence of this mountain range is " + maxProminence);
         return maxProminence;
